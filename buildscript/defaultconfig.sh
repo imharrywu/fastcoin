6a5d5448ef4a600a4770234059d509fb866257dd
Harry Wu <imharrywu@gmail.com>
2014-11-11 18:20:33 +0800

    add building utilities.

---
+++ buildscript/defaultconfig.sh
@@ -0,0 +1,73 @@
+export DEPEND_HOME=/d/workspace/github/altcoin/bitcoin-3rd
+
+export MAKENSIS_HOME=${DEPEND_HOME}/nsis-2.46
+#Notice, use the cygwin hexdump tool to test.
+#http://gnuwin32.sourceforge.net/packages/util-linux-ng.htm
+export HEXTOOLS_BIN_HOME=${DEPEND_HOME}/hexdump/bin
+export GIT_BIN_HOME="/c/Git/bin"
+
+export BDB_INC_HOME=${DEPEND_HOME}/BerkeleyDB-4.8/include
+export BDB_LIB_HOME=${DEPEND_HOME}/BerkeleyDB-4.8/lib
+
+export BOOST_TOP_HOME=${DEPEND_HOME}/boost-1.55
+export BOOST_INC_HOME=${DEPEND_HOME}/boost-1.55/include/boost-1_55
+export BOOST_LIB_HOME=${DEPEND_HOME}/boost-1.55/lib
+
+export PNG_INC_HOME=${DEPEND_HOME}/libpng-1.6.9/include
+export PNG_LIB_HOME=${DEPEND_HOME}/libpng-1.6.9/lib
+
+export MINIUPNPC_INC_HOME=${DEPEND_HOME}/miniupnpc-1.9.0/include
+export MINIUPNPC_LIB_HOME=${DEPEND_HOME}/miniupnpc-1.9.0/lib
+
+export PROTOC_BIN_HOME=${DEPEND_HOME}/protobuf-2.5.0/bin
+export PROTOC_INC_HOME=${DEPEND_HOME}/protobuf-2.5.0/include
+export PROTOC_LIB_HOME=${DEPEND_HOME}/protobuf-2.5.0/lib
+
+export QR_INC_HOME=${DEPEND_HOME}/qrencode-3.4.3/include
+export QR_LIB_HOME=${DEPEND_HOME}/qrencode-3.4.3/lib
+
+export QT_BIN_HOME=${DEPEND_HOME}/qt-4.8.5-mingw32-4.8.1/bin
+export QT_INC_HOME=${DEPEND_HOME}/qt-4.8.5-mingw32-4.8.1/include
+export QT_LIB_HOME=${DEPEND_HOME}/qt-4.8.5-mingw32-4.8.1/lib
+export QT_PLUGIN_HOME=${DEPEND_HOME}/qt-4.8.5-mingw32-4.8.1/plugins
+export QT_CODECS_HOME=${DEPEND_HOME}/qt-4.8.5-mingw32-4.8.1/plugins/codecs
+export QT_ACCESSIBLE_HOME=${DEPEND_HOME}/qt-4.8.5-mingw32-4.8.1/plugins/accessible
+
+export SSL_INC_HOME=${DEPEND_HOME}/ssl/include
+export SSL_LIB_HOME=${DEPEND_HOME}/ssl/lib
+
+export CRYPTO_INC_HOME=${DEPEND_HOME}/ssl/include
+export CRYPTO_LIB_HOME=${DEPEND_HOME}/ssl/lib
+
+export ZLIB_INC_HOME=${DEPEND_HOME}/zlib-1.2.8/include
+export ZLIB_LIB_HOME=${DEPEND_HOME}/zlib-1.2.8/lib
+
+export PATH=${PATH}:${MAKENSIS_HOME}:${GIT_BIN_HOME}:${HEXTOOLS_BIN_HOME}
+
+./configure \
+	--enable-wallet \
+	--disable-tests \
+	--disable-ccache \
+	--enable-debug \
+	--enable-tests \
+	--enable-upnp-default \
+	--with-boost="${BOOST_TOP_HOME}" \
+	--with-qrencode=yes \
+	--with-miniupnpc=yes \
+	--with-protoc-bindir=${PROTOC_BIN_HOME} \
+	--with-daemon=yes \
+	--with-gui=qt4 \
+	--with-qt-incdir=${QT_INC_HOME} \
+	--with-qt-libdir=${QT_LIB_HOME} \
+	--with-qt-bindir=${QT_BIN_HOME} \
+	--with-qt-plugindir=${QT_PLUGIN_HOME} \
+	CPPFLAGS="-I${QR_INC_HOME} -I${PNG_INC_HOME} -I${PROTOC_INC_HOME} -I${SSL_INC_HOME} -I${CRYPTO_INC_HOME} -I${BDB_INC_HOME} -I${MINIUPNPC_INC_HOME} -I${BOOST_INC_HOME} -I${ZLIB_INC_HOME}" \
+	CFLAGS="-w -g -O0 " \
+	LDFLAGS="-L${QR_LIB_HOME} -L${QT_CODECS_HOME} -L${QT_ACCESSIBLE_HOME} -L${PNG_LIB_HOME} -L${PROTOC_LIB_HOME} -L${SSL_LIB_HOME} -L${CRYPTO_LIB_HOME} -L${BDB_LIB_HOME} -L${MINIUPNPC_LIB_HOME} -L${ZLIB_LIB_HOME}" 
+
+#sh ./unix_path_to_win_path.sh /d/workspace
+	
+#make # will use the above path env.
+
+#echo @@@@@Remeber to export path for Git@@@@@@@@@@@@@@@
+
---
+++ genesis/genesis.py
@@ -0,0 +1,207 @@
+import hashlib, binascii, struct, array, os, time, sys, optparse
+import scrypt
+
+from construct import *
+
+
+def main():
+  options = get_args()
+
+  algorithm = get_algorithm(options)
+
+  # https://en.bitcoin.it/wiki/Difficulty
+  bits, target   = get_difficulty(algorithm)
+
+  input_script  = create_input_script(options.timestamp)
+  output_script = create_output_script(options.pubkey)
+  # hash merkle root is the double sha256 hash of the transaction(s) 
+  tx = create_transaction(input_script, output_script,options)
+  hash_merkle_root = hashlib.sha256(hashlib.sha256(tx).digest()).digest()
+  print_block_info(options, hash_merkle_root, bits)
+
+  block_header        = create_block_header(hash_merkle_root, options.time, bits, options.nonce)
+  genesis_hash, nonce = generate_hash(block_header, algorithm, options.nonce, target)
+  announce_found_genesis(genesis_hash, nonce)
+
+
+def get_args():
+  parser = optparse.OptionParser()
+  parser.add_option("-t", "--time", dest="time", default=int(time.time()), 
+                   type="int", help="the (unix) time when the genesisblock is created")
+  parser.add_option("-z", "--timestamp", dest="timestamp", default="The Times 03/Jan/2009 Chancellor on brink of second bailout for banks",
+                   type="string", help="the pszTimestamp found in the coinbase of the genesisblock")
+  parser.add_option("-n", "--nonce", dest="nonce", default=0,
+                   type="int", help="the first value of the nonce that will be incremented when searching the genesis hash")
+  parser.add_option("-a", "--algorithm", dest="algorithm", default="SHA256",
+                    help="the PoW algorithm: [SHA256|scrypt|X11|X13|X15]")
+  parser.add_option("-p", "--pubkey", dest="pubkey", default="04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f",
+                   type="string", help="the pubkey found in the output script")
+  parser.add_option("-v", "--value", dest="value", default=5000000000,
+                   type="int", help="the value in coins for the output, full value (exp. in bitcoin 5000000000 - To get other coins value: Block Value * 100000000)")
+
+  (options, args) = parser.parse_args()
+  return options
+
+def get_algorithm(options):
+  supported_algorithms = ["SHA256", "scrypt", "X11", "X13", "X15"]
+  if options.algorithm in supported_algorithms:
+    return options.algorithm
+  else:
+    sys.exit("Error: Given algorithm must be one of: " + str(supported_algorithms))
+
+def get_difficulty(algorithm):
+  if algorithm == "scrypt":
+    return 0x1e0ffff0, 0x0ffff0 * 2**(8*(0x1e - 3))
+  elif algorithm == "SHA256":
+    return 0x1d00ffff, 0x00ffff * 2**(8*(0x1d - 3)) 
+  elif algorithm == "X11" or algorithm == "X13" or algorithm == "X15":
+    return 0x1e0ffff0, 0x0ffff0 * 2**(8*(0x1e - 3))
+
+def create_input_script(psz_timestamp):
+  psz_prefix = ""
+  #use OP_PUSHDATA1 if required
+  if len(psz_timestamp) > 76: psz_prefix = '4c'
+
+  script_prefix = '04ffff001d0104' + psz_prefix + chr(len(psz_timestamp)).encode('hex')
+  print (script_prefix + psz_timestamp.encode('hex'))
+  return (script_prefix + psz_timestamp.encode('hex')).decode('hex')
+
+
+def create_output_script(pubkey):
+  script_len = '41'
+  OP_CHECKSIG = 'ac'
+  return (script_len + pubkey + OP_CHECKSIG).decode('hex')
+
+
+def create_transaction(input_script, output_script,options):
+  transaction = Struct("transaction",
+    Bytes("version", 4),
+    Byte("num_inputs"),
+    StaticField("prev_output", 32),
+    UBInt32('prev_out_idx'),
+    Byte('input_script_len'),
+    Bytes('input_script', len(input_script)),
+    UBInt32('sequence'),
+    Byte('num_outputs'),
+    Bytes('out_value', 8),
+    Byte('output_script_len'),
+    Bytes('output_script',  0x43),
+    UBInt32('locktime'))
+
+  tx = transaction.parse('\x00'*(127 + len(input_script)))
+  tx.version           = struct.pack('<I', 1)
+  tx.num_inputs        = 1
+  tx.prev_output       = struct.pack('<qqqq', 0,0,0,0)
+  tx.prev_out_idx      = 0xFFFFFFFF
+  tx.input_script_len  = len(input_script)
+  tx.input_script      = input_script
+  tx.sequence          = 0xFFFFFFFF
+  tx.num_outputs       = 1
+  tx.out_value         = struct.pack('<q' ,options.value)#0x000005f5e100)#012a05f200) #50 coins
+  #tx.out_value         = struct.pack('<q' ,0x000000012a05f200) #50 coins
+  tx.output_script_len = 0x43
+  tx.output_script     = output_script
+  tx.locktime          = 0 
+  return transaction.build(tx)
+
+
+def create_block_header(hash_merkle_root, time, bits, nonce):
+  block_header = Struct("block_header",
+    Bytes("version",4),
+    Bytes("hash_prev_block", 32),
+    Bytes("hash_merkle_root", 32),
+    Bytes("time", 4),
+    Bytes("bits", 4),
+    Bytes("nonce", 4))
+
+  genesisblock = block_header.parse('\x00'*80)
+  genesisblock.version          = struct.pack('<I', 1)
+  genesisblock.hash_prev_block  = struct.pack('<qqqq', 0,0,0,0)
+  genesisblock.hash_merkle_root = hash_merkle_root
+  genesisblock.time             = struct.pack('<I', time)
+  genesisblock.bits             = struct.pack('<I', bits)
+  genesisblock.nonce            = struct.pack('<I', nonce)
+  return block_header.build(genesisblock)
+
+
+# https://en.bitcoin.it/wiki/Block_hashing_algorithm
+def generate_hash(data_block, algorithm, start_nonce, target):
+  print 'Searching for genesis hash..'
+  nonce           = start_nonce
+  last_updated    = time.time()
+  difficulty      = float(0xFFFF) * 2**208 / target
+  update_interval = int(1000000 * difficulty)
+
+  while True:
+    sha256_hash, header_hash = generate_hashes_from_block(data_block, algorithm)
+    last_updated             = calculate_hashrate(nonce, update_interval, difficulty, last_updated)
+    if is_genesis_hash(header_hash, target):
+      if algorithm == "X11" or algorithm == "X13" or algorithm == "X15":
+        return (header_hash, nonce)
+      return (sha256_hash, nonce)
+    else:
+     nonce      = nonce + 1
+     data_block = data_block[0:len(data_block) - 4] + struct.pack('<I', nonce)  
+
+
+def generate_hashes_from_block(data_block, algorithm):
+  sha256_hash = hashlib.sha256(hashlib.sha256(data_block).digest()).digest()[::-1]
+  header_hash = ""
+  if algorithm == 'scrypt':
+    header_hash = scrypt.hash(data_block,data_block,1024,1,1,32)[::-1] 
+  elif algorithm == 'SHA256':
+    header_hash = sha256_hash
+  elif algorithm == 'X11':
+    try:
+      exec('import %s' % "xcoin_hash")
+    except ImportError:
+      sys.exit("Cannot run X11 algorithm: module xcoin_hash not found")
+    header_hash = xcoin_hash.getPoWHash(data_block)[::-1]
+  elif algorithm == 'X13':
+    try:
+      exec('import %s' % "x13_hash")
+    except ImportError:
+      sys.exit("Cannot run X13 algorithm: module x13_hash not found")
+    header_hash = x13_hash.getPoWHash(data_block)[::-1]
+  elif algorithm == 'X15':
+    try:
+      exec('import %s' % "x15_hash")
+    except ImportError:
+      sys.exit("Cannot run X15 algorithm: module x15_hash not found")
+    header_hash = x15_hash.getPoWHash(data_block)[::-1]
+  return sha256_hash, header_hash
+
+
+def is_genesis_hash(header_hash, target):
+  return int(header_hash.encode('hex_codec'), 16) < target
+
+
+def calculate_hashrate(nonce, update_interval, difficulty, last_updated):
+  if nonce % update_interval == update_interval - 1:
+    now             = time.time()
+    hashrate        = round(update_interval/(now - last_updated))
+    generation_time = round(difficulty * pow(2, 32) / hashrate / 3600, 1)
+    sys.stdout.write("\r%s hash/s, estimate: %s h"%(str(hashrate), str(generation_time)))
+    sys.stdout.flush()
+    return now
+  else:
+    return last_updated
+
+
+def print_block_info(options, hash_merkle_root, bits):
+  print "algorithm: "    + (options.algorithm)
+  print "merkle hash: "  + hash_merkle_root[::-1].encode('hex_codec')
+  print "pszTimestamp: " + options.timestamp
+  print "pubkey: "       + options.pubkey
+  print "time: "         + str(options.time)
+  print "bits: "         + str(hex(bits))
+
+
+def announce_found_genesis(genesis_hash, nonce):
+  print "genesis hash found!"
+  print "nonce: "        + str(nonce)
+  print "genesis hash: " + genesis_hash.encode('hex_codec')
+
+
+# GOGOGO!
+main()
