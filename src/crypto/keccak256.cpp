fa6eda5b97c469f7785d5df0e4c75bd724499aa5
harrywu <imharrywu@gmail.com>
2014-12-11 12:55:38 +0800

    add Keccak pow module.

---
+++ src/crypto/keccak256.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2012 NIST, Keccak teams
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file was originally written by Colin Percival as part of the Tarsnap
+ * online backup system.
+ */
+
+#include "keccak256.h"
+#include "KeccakCodePackage/Modes/KeccakHash.h"
+#include "util.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+////// CKeccak_256
+
+CKeccak_256::CKeccak_256()
+{
+    m_vchBuf.reserve(80); // most common used in block PoW, and block head is 80 bytes.
+}
+
+CKeccak_256& CKeccak_256::Write(const unsigned char* data, size_t len)
+{
+    for (size_t i = 0; i < len; ++i) m_vchBuf.push_back(data[i]);
+    return *this;
+}
+
+void CKeccak_256::Finalize(unsigned char hash[OUTPUT_SIZE])
+{
+    
+}
+
+CKeccak_256& CKeccak_256::Reset()
+{
+    m_vchBuf.clear();
+    m_vchBuf.reserve(80);
+    return *this;
+}
---
+++ src/crypto/keccak256.h
@@ -0,0 +1,21 @@
+#ifndef SCRYPT_H
+#define SCRYPT_H
+#include <stdlib.h>
+#include <stdint.h>
+#include <vector>
+
+/** A hasher class for SHA3-256. */
+class CKeccak_256
+{
+private:
+    std::vector<unsigned char> m_vchBuf;
+public:
+    static const size_t OUTPUT_SIZE = 32;
+
+    CKeccak_256();
+    CKeccak_256& Write(const unsigned char* data, size_t len);
+    void Finalize(unsigned char hash[OUTPUT_SIZE]);
+    CKeccak_256& Reset();
+};
+
+#endif
