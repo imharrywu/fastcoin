f096cc2103662b2459aff0f245e903b656652074
harrywu <imharrywu@gmail.com>
2014-12-24 10:02:29 +0800

    default to sha256 double mining, version 2

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -24,7 +24,7 @@ class CBlockHeader
 {
 public:
     // header
-    static const int32_t CURRENT_VERSION=5;
+    static const int32_t CURRENT_VERSION=2;
     int32_t nVersion;
     uint256 hashPrevBlock;
     uint256 hashMerkleRoot;
